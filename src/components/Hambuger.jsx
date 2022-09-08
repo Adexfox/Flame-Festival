03f8561bd113ebf7bc39466c9b7e1ee2e604b2b7
adexfox <adewale199@gmail.com>
2022-09-08 23:56:46 +0100

    :recycle: Refactor

--- src/components/Hambuger.jsx
+++ src/components/Hambuger.jsx
@@ -1,49 +1,69 @@
 import React from 'react';
 // import { Menu } from '../interfaces/menuItemsInterface';
 import { items } from './menuItems';
-const menu = document.querySelector('.menua');
-const hambuger = document.querySelector('.menua-burger');
+// const menu = document.querySelector('.menu');
+// const hambuger = document.querySelector('.menu-burger');
+// const nav = document.querySelector('.nav');
+// const menuNav = document.querySelector('.menu-nav');
+// const navItems = document.querySelectorAll('.menu-nav__item');
+// 
+// 
+// let showMenu = false;
+// 
+// menu.addEventListener('click', toggleMenu);
+// 
+// function toggleMenu () {
+//   
+//   if (!showMenu) {
+//     hambuger.classList.add('open');
+//     nav.classList.add('open');
+//     menuNav.classList.add('open');
+//     navItems.forEach(item => item.classList.add('open'));
+// 
+//     showMenu = true;
+//   }
+//   else {
+//     hambuger.classList.remove('open');
+//      nav.classList.remove('open');
+//     menuNav.classList.remove('open');
+//     navItems.forEach(item => item.classList.remove('open'));
+//     
+//     showMenu = false;
+//   }
+// }
+const menu = document.querySelector('.menu');
+const hamburger = document.querySelector('.menu-burger');
 const nav = document.querySelector('.nav');
-const menuNav = document.querySelector('.menua-nav');
-const navBtn = document.querySelector('.navButtonItem');
+const menuNav = document.querySelector('.menu-nav');
 
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('open');
+  menu.classList.toggle('open');
+  nav.classList.toggle('open');
+  // menuNav.classList.toggle('open'); 
+})
+document.querySelectorAll('.menu-nav__item').forEach(n => 
+  n.addEventListener('click', () => {
+    hamburger.classList.remove('open');
+    menu.classList.remove('open');
+    nav.classList.remove('open');
+    // menuNav.classList.remove('open');
+  }))
 
-let showMenu = false;
-
-menu.addEventListener('click', toggleMenu);
-
-function toggleMenu () {
-  
-  if (!showMenu) {
-    hambuger.classList.add('open');
-    nav.classList.add('open');
-    menuNav.classList.add('open');
-    navBtn.classList.remove('open');
-
-    showMenu = true;
-  }
-  else {
-    hambuger.classList.remove('open');
-     nav.classList.remove('open');
-    menuNav.classList.remove('open');
-    navBtn.classList.add('open');
-    showMenu = false;
-  }
-}
 
 const Hambuger = () => {
   return (
     <>
-      <div className="menua">
-        <span className="menua-burger"></span>
+      <div className="menu">
+        <span className="menu-burger"></span>
       </div>
       <nav className='nav'>
-        <ul className='menua-nav'>
+        <ul className='menu-nav'>
             {items.map( (props) => {
               const {id, text, url} = props;
               return(
-                <li className='menua-nav__item'>
-                    <a href={url} key={id} className='menua-nav__link'>
+                <li className='menu-nav__item'>
+                    <a href={url} key={id} className='menu-nav__link'>
                       {text}
                     </a>
                </li>
