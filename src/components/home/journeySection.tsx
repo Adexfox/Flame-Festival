9a6ad932a5304052b1d000353561f00dd69d0f45
adexfox <adewale199@gmail.com>
2022-10-15 23:53:40 +0100

    :necktie: Home Final Details

---
+++ src/components/home/journeySection.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+type Props = {}
+
+const JourneySection = (props: Props) => {
+  
+  return (
+    <>
+      <section className='journey-section-1' >
+        <div className="fifth-cut">
+          <img src="images/top-cut.png" alt="" />
+        </div>
+        <section className="journey-section-1-text">
+          <h3>The Journey</h3>
+          <h1>THIS JOURNEY BEGINS WITH A DEPATURE <br />FROM THE EVERYDAY</h1>
+          <p>Less than an hour from Miami, reaching the island is a breeze via private aircraft alonside a handful of fellow</p>
+          <p>travelers. The flight is short, but you never know who you might meet on the way...</p>
+
+          <button className='journey-btn'>Get Tickets</button>
+        </section>
+        <div className="sixth-cut">
+          <img src="images/top-cut.png" alt="" />
+        </div>
+        <div className="model">
+          <img src="images/glasses-bikini1.png" alt="" />
+        </div>
+      </section>
+      <section className='journey-section-2'>
+        <video src='videos/map.mp4' muted loop autoPlay />
+      </section>
+    </>
+  )
+}
+
+export default JourneySection;
---
+++ src/components/home/packageSection.tsx
@@ -0,0 +1,47 @@
+import React, { useEffect } from 'react';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
+
+type Props = {}
+
+const PackageSection = (props: Props) => {
+    useEffect(() => {
+      AOS.init({duration: 3000});
+    //   console.log('initiate');
+    }
+    
+    )
+  return (
+    <>
+        <section className="package-section-1">
+            <div className='seventh-cut'>
+                <img src="images/top-cut.png" alt="" />
+            </div>
+            <div className='flower-1'>
+                <img src="images/flower-1.png" alt="" data-aos='slide-up' data-aos-delay="50"/>
+            </div>
+            <div className='flower-7'>
+                <img src="images/flower-7.png" alt="" data-aos='slide-down' data-aos-delay="50"/>
+            </div>
+            <div className="package-section-1-text">
+                <h3 data-aos='slide-down' data-aos-delay="50">The <span><span>P</span>ackages</span></h3>
+                <h1 data-aos='slide-down' data-aos-delay="50">JOIN US</h1>
+                <p data-aos='slide-down' data-aos-delay="50">Your Flame ticket encompasses all flights, culinary delights and accomodations.</p>
+                <p data-aos='slide-down' data-aos-delay="50">General Admission is your key to the expirience, but VIP unlocks another level of possibilities. </p>
+                <p data-aos='slide-down' data-aos-delay="50"> From backstage partires to custom expriences. VIP offers uncompromising access to the best of the best</p>
+                <button className="package-btn" data-aos='slide-down' data-aos-delay="50">Get Tickets</button>
+                
+            </div>
+            <div className='eighth-cut'>
+                <img src="images/top-cut.png" alt="" />
+            </div>
+        </section>
+        <section className="package-section-2">
+
+        </section>
+    </>
+  ) 
+}
+
+export default PackageSection;
