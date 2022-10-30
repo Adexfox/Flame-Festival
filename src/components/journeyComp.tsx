d505daf24aa07a314c92f02b63f2d2ee3f853445
adexfox <adewale199@gmail.com>
2022-10-30 02:13:05 +0100

    :stethoscope: Styled Journey

--- src/components/journeyComp.tsx
+++ src/components/journeyComp.tsx
@@ -1,37 +1,52 @@
 import React from 'react';
-import Hamburger from '../components/Hamburger';
+import { IconContext } from 'react-icons/lib/esm/iconContext';
+import { FaTwitter } from 'react-icons/fa';
+import { FaFacebookF } from 'react-icons/fa';
+import { RiInstagramFill } from 'react-icons/ri';
+import { GrMail } from 'react-icons/gr';
 
 type Props = {}
 
 const JourneyComp = (props: Props) => {
  return(
-    <>
-        <Hamburger />
-        <section>
+     <IconContext.Provider value={{ color: '#000', style: {marginRight:  '2rem'}, className: 'social'}}>
+        <main className='journey-page'>
+          <div className='header'>
+            <h1>Getting Here</h1>
+          </div>
           <div className='journey-plane'>
               <img src="images/Journey_Plane.png" alt="" />
           </div>
           <div className='journey-body'>
-
+            <p>
+              All pages include complimentary flights to and from the Miami. Flights will depart to the Miami from Bahamas on Thursday and Friday, and return to Miami from the <br />Bahamas on Sunday and Monday. You may choose any combination of flights. For example you could leave for the Bahamas Friday morning and return to Miami on Monday <br />morning, or leave Friday evening and return Sunday evening. We will do our best to accommodate your preferences. Flight departure times will be schedule on first <br />come first served basis after you have completed your preference form. Your traveler preference form will be sent to you within a few days after you purchase <br />your ticket. If You have already purchased a ticket package, please make sure everyone in your travel party fills out a traveler profile form. This will expedite your flight <br />assignment. For more information on travel, please visit our FAQ. <br /><br />
+              <span className='hashtag'>#FyreFestival</span>
+            </p>
           </div>
           <div className="journey-map">
             <img src="images/map.jpg" alt="" />
           </div>
-        </section>
-        <footer>
-          <div className='social'>
-            
-          </div>
-          <div className='zigzag'>
-            <img src="images/zigzag.png" alt="" />
-          </div>
-          <div className='nav-cta'>
-              <a href="">Contact |</a>
-              <a href=""> FAQ</a>
-              
-          </div>
-        </footer>
-    </>
+         
+          <footer>
+            <div className='journey-social'>
+              <FaFacebookF />
+              <FaTwitter />
+              <RiInstagramFill />
+              <GrMail />
+            </div>
+            <div className='zigzag'>
+              <img src="images/zigzag.png" alt="" />
+            </div>
+            <div className='nav-cta'>
+                <a href="/">Home</a>
+                <a href="/contact">Contact</a>
+                <a href="/faq">FAQ</a>
+                
+            </div>
+          </footer>
+        </main>
+        
+     </IconContext.Provider>
   ) 
 
 }
